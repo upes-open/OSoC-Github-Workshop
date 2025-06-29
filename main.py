ae40aa1401fe9ffdf146f48620fae8e186daeac6
Sushmeta1 <sushmetanegi@gmail.com>
2025-06-29 18:03:54 +0530

    Sushmeta Negi: main.py

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-<<<<<<< HEAD
-
-=======
 rows = int(input("Enter the number of rows: "))
 
 for i in range(1, rows + 1):
     for j in range(1, i + 1):
         print("*", end="")
     print()
->>>>>>> 5b2ceaa (Initialize commit)
