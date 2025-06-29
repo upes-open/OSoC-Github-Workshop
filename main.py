64325b8f9aef3a7beac2e101fd5e62d177078bf3
Kapoor-0905 <500097111@stu.upes.ac.in>
2025-06-29 18:21:02 +0530

    ADDED: line

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 rows = int(input("Enter the number of rows: "))
-
+print("THIS IS A RIGHT ANGLE TRIANGLE PATTERN")
 for i in range(1,rows+1):
     for j in range(1, i+1):
         print("*", end="")
