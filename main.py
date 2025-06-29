02c6aec9e791b0f13b67f1b686e445b4e68b7ba3
Soumya Jain <151627938+soumya-jain123@users.noreply.github.com>
2025-06-29 18:04:59 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,3 @@
-<<<<<<< HEAD
-rows = int(input("Enter the number of rows: "))
-columns = int(input("Enter the number of columns: "))
-
-for i in range(rows):
-    for j in range(columns):
-        if i == 0 or i == rows - 1 or j == 0 or j == columns - 1:
-            print("*", end=" ")
-        else:
-            print(" ", end=" ")
-    print()
-=======
-# This is used to create a triangle with * pattern
-
-
 rows = int(input("Enter the number of rows: "))
 columns = int(input("Enter the number of columns: "))
 
@@ -22,5 +7,4 @@ for i in range(1, rows + 1):
             print("*", end=" ")
         else:
             print(" ", end=" ")
-    print()  # Move to the next line after each row
->>>>>>> df65c41b6269023bb1500aa92dd4cbce7eff2ba3
+    print()
